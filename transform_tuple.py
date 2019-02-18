77a6b9b6d2e0f7037b2a55c245499c9c681b08f6
cbur24 <c.a.burton123@gmail.com>
2019-02-19 09:34:24 +1100

    add projection object

--- transform_tuple.py
+++ transform_tuple.py
@@ -1,10 +1,12 @@
-def transform_tuple(ds, coords, rotation=0.0):
+def transform_tuple(ds, coords, epsg=3577, rotation=0.0):
     """
-    Creates a transform tuple from an xarray object for use with GDAL
+    Creates a transform tuple from an xarray object for use with GDAL, along with
+    a projection object in the form of WKT.
 
     ds = xarray dataset or dataArray
     coords = tuple. The georeferencing coordinate data. e.g (ds.long,ds.lat) or (ds.x,ds.y)
                     Order MUST BE X then Y
+    epsg = the 
     rotation = the degrees of rotation of the image. If North up, rotation = 0.0
     
     """
@@ -15,4 +17,9 @@ def transform_tuple(ds, coords, rotation=0.0):
 
     transform = (east, EW_pixelRes, rotation, north, rotation, NS_pixelRes)
     
-    return transform
+    import osr
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(epsg)
+    prj_wkt = srs.ExportToWkt()
+    
+    return transform, projection
